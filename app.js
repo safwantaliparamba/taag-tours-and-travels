4a9e04807d8562cd80ac319608a06908dea05f55
safwantaliparamba <safwansafu242090@gmail.com>
2022-01-08 11:49:16 +0530

    form submission added

--- app.js
+++ app.js
@@ -15,3 +15,26 @@ $('.navTrigger').click(function () {
     $("#mainListDiv").fadeIn();
 
 });
+
+// ajax email submission
+
+var form = document.getElementById("enquiry");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    status.innerHTML = "Thanks for your submission!";
+    form.reset()
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
