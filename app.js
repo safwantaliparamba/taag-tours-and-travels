31187bd3ba9ea8e96502b0e07e445094197b771f
safwantaliparamba <safwansafu242090@gmail.com>
2022-01-08 10:38:54 +0530

    contact us section added

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('.nav').addClass('affix');
+        console.log("OK");
+    } else {
+        $('.nav').removeClass('affix');
+    }
+});
+
+
 $('.navTrigger').click(function () {
     $(this).toggleClass('active');
     console.log("Clicked menu");
